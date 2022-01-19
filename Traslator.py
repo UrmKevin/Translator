49496a771fd2fc574547f2d45134358b018e560e
Kevin <kevinurm2005@gmail.com>
2022-01-19 14:13:41 +0200

    Update*

--- Traslator.py
+++ Traslator.py
@@ -1,11 +1,37 @@
 from module1 import *
 
+rus_list=[]
+eng_list=[]
+rus_list=loe_failist("rus.txt",rus_list)
+eng_list=loe_failist("eng.txt",eng_list)
+
 while 1:
-    way=int(input("1 - Eng to Rus\n2 - Rus to Eng\n3 - end task\n"))
-    if way==1:
-        eng_to_rus('Eng.txt','Rus.txt')
-        print("_____")
-    elif way==2:
-        print("rustoeng")
-    elif way==3:
+    menu=input("\nRääkida -R,\nKõik sõnad -S,\nTõlgida -T,\nUus sõna -U,\nViga -V,\nKontroll -K,\nLõpp -L\n--> ")
+    if menu.upper()=="U":
+        rus_list=uus_sona("rus.txt",input("Новое слово: "))
+        eng_list=uus_sona("eng.txt",input("New word: "))
+
+    elif menu.upper()=="S":
+        print(rus_list)
+        print(eng_list)
+
+    elif menu.upper()=="T":
+        tolkimine(rus_list,eng_list)
+
+    elif menu.upper()=="V":
+        viga(rus_list,eng_list,"rus.txt","eng.txt")
+
+    elif menu.upper()=="R":
+        keel=input("Mis keeles ütelda?")
+        sona=input("Sõna: ")
+        heli(sona,keel)
+
+    elif menu.upper()=="L":
         break
+
+    elif menu.upper()=="K":
+        kontroll(eng_list)
+
+    else:
+        break
+
--- module1.py
+++ module1.py
@@ -1,15 +1,90 @@
-def loe_failist(f):
+def loe_failist(f:str,l:list):
+    """Info failist f listisse l
+    """
     fail=open(f,'r',encoding="utf-8-sig")
-    mas=[] 
     for rida in fail:
-        mas.append(rida.strip())
+        l.append(rida.strip())
     fail.close()
-    return mas
+    return l
+
+def failisse_salvestamine(f:str,l:list):
+    """Loetelu salvestame failisse
+    """
+    fail=open(f,'w')
+    for el in l:
+        fail.write(el+'\n')
+    fail.close()
+
+def rida_salvestamine(f:str,rida:str):
+    """Üks sõna või lause(rida) salvestame failisse
+    :param str f: fail kuku salvestame
+    :param str rida: 
+    """
+    fail=open(f,'a')
+    fail.write(rida+'\n')
+    fail.close()
+
+def uus_sona(f:str,rida:str)->list:
+    """Lisame uus sõna failisse ja listisse
+    :param str f: Faili nimetus
+    :param str rida: lisatav sõna
+    """
+    #l=[]
+    with open(f,"a",encoding="utf-8-sig") as fail:
+        fail.write(rida+"\n")
+    #l=loe_failist(f)
+    #return l
+
+def correction(sona:str,l:list):
+    for i in range(len(l)):
+        if l[i]==sona:
+            uus_sona=sona.replace(sona,input("Uus sõna"))
+            l.insert(i,uus_sona)
+            l.remove(sona)
+    return l
+
+#def failisse():
+
+def tolkimine(l1:list,l2:list):
+    """
+    """
+    sona=input("Mida tõlkida: ")
+    if sona in l1:
+        tolk=l2[l1.index(sõna)]
+        print(sona+" - "+tolk)
+    elif sona in l2:
+        tolk=l1[l2.index(sona)]
+        print(sona+" - "+tolk)
+    else:
+        print("Sõna puudub sõnastikus")
+
+def viga(l1:list,l2:list,f1:str,f2:str):
+    sona=input("Sõna veaga? ")
+    if sona not in l1 and sona not in l2:
+        print("Sõna puudub")
+    else:
+        if sona in l1:
+            tolk=l2[l1.index(sona)]
+            l1.remove(sona)
+            l2.remove(tolk)
+        if sona in l2:
+            tolk=l1[l2.index(sona)]
+            l1.remove(sona)
+            l2.remove(tolk)
+        l1.append(input("Введи новое слово: "))
+        l2.append(input("Sisesta uus sõna: "))
+        failisse_salvestamine(f1,l1)
+        failisse_salvestamine(f2,l2)
 
 
-def eng_to_rus(e,r):
-    word=input("Write your word - ")
-    eng=loe_failist('Eng.txt')
-    if word in eng:
-        print(r[1])
+import os
+from gtts import gTTS
+from random import *
 
+def heli(text:str,keel:str):
+    obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3")
+    os.system("heli.mp3")
+
+
+def kontroll(e):
+    sona=input(Translate"e[randint(0,5)])
